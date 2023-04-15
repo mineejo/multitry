aba952f18191f9de6da00e4a3803fb9521bada46
mineejo <evgenyshaimov@gmail.com>
2023-04-15 21:11:38 +0300

    feat(package): jsx support

--- lib/index.ts
+++ lib/index.ts
@@ -41,3 +41,36 @@ export function multitry<Type>(
   }
   return;
 }
+
+/**
+ * The Multitry function is a wrapper for multitry that allows for
+ * try, catch, and finally blocks in jsx/tsx.
+ *
+ * @param {{
+ *     try: () => string;
+ *     catch?: (error: Error) => Error | string;
+ *     finally?: () => void;
+ *   }[]} blocks - blocks is an array of objects, where each object
+ *   represents a function to be executed in a try-catch-finally block.
+ *   Each object should have a try property, which is a function that
+ *   will be executed in the try block. It can also have an optional
+ *   catch property.
+ *
+ * @returns The `Multitry` function is returning the result of calling
+ * the `multitry` function with an object containing a `try` function,
+ * a `catch` function (if provided), and a `finally` function
+ * (if provided). The `try` function is the only required
+ * property and is expected to return a string.
+ * The `catch` function, if provided, takes an `Error
+ */
+export function Multitry(blocks: {
+  try: () => string;
+  catch?: (error: Error) => Error | string;
+  finally?: () => void;
+}) {
+  return multitry({
+    try: () => blocks["try"](),
+    catch: (e) => (blocks?.["catch"] ? blocks["catch"](e) : e.toString()),
+    finally: () => (blocks?.["finally"] ? blocks["finally"]() : ""),
+  });
+}
