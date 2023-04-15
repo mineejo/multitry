c80627d44843a182c47f2c8e75c17c921464bf9d
mineejo <evgenyshaimov@gmail.com>
2023-04-15 17:57:51 +0300

    feat: update jsdoc

--- lib/index.ts
+++ lib/index.ts
@@ -1,28 +1,36 @@
 /**
- * Functional try-catch-finally construction wrapper.
+ * The multitry function is a wrapper for a functional try-catch-finally
+ * construction that returns a value or undefined in case of an error
+ * without handling the Error object.
  *
- * @param construct - Contains construct functions.
- * @param construct.try - Returns value, works like normal.
- * @param [construct.catch] - Includes an error argument, returns an `Error` or value when handled.
- * @param [construct.finally] - Does return void, works like normal.
+ * @param {{
+ *     try: () => Type;
+ *     catch?: (error: Error) => Error | Type;
+ *     finally?: () => void;
+ *   }[]} blocks - blocks is an array of objects, where each object
+ *   represents a function to be executed in a try-catch-finally block.
+ *   Each object should have a try property, which is a function that
+ *   will be executed in the try block. It can also have an optional
+ *   catch property.
  *
- * @returns a value or `undefined`, in case of an error without handling the `Error` object.
+ * @returns The multitry function can return a value of type Type,
+ * an Error object, or undefined.
  */
 export function multitry<Type>(
-  ...construct: {
+  ...blocks: {
     try: () => Type;
     catch?: (error: Error) => Error | Type;
     finally?: () => void;
   }[]
 ): Type | Error | undefined {
-  for (const c of construct) {
+  for (const block of blocks) {
     try {
-      const result = c.try();
+      const result = block.try();
       if (result) return result;
     } catch (error) {
-      if (c.catch) return c.catch(error) ?? error;
+      if (block.catch) return block.catch(error) ?? error;
     } finally {
-      if (c.finally) c.finally();
+      if (block.finally) block.finally();
     }
   }
   return;
