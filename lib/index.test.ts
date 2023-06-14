009c410710c79b6d3d5d176b43dc5ea264519685
mineejo <mineejo.dev@gmail.com>
2023-06-14 14:27:51 +0300

    chore: update tests

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,17 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { wrap } from "./index.ts";
+import { wrap } from "./index.js";
+import test from "ava";
 
-test("wrap: return result", () => {
+test("return result", (t) => {
   const result = wrap({
     try: () => "Hello world!",
   });
 
   const message = "Hello world!";
-  expect(result).toBe(message);
+  t.is(result, message);
 });
 
-test("wrap: async return result", async () => {
+test("async return result", async (t) => {
   const result = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(
@@ -23,10 +22,10 @@ test("wrap: async return result", async () => {
   });
 
   const message = "Hello world!";
-  expect(result).toBe(message);
+  t.is(result, message);
 });
 
-test("wrap: multi return result", () => {
+test("multi return result", (t) => {
   const result = wrap(
     {
       try: () => undefined,
@@ -37,10 +36,10 @@ test("wrap: multi return result", () => {
   );
 
   const message = "Hello world!";
-  expect(result).toBe(message);
+  t.is(result, message);
 });
 
-test("wrap: return error", () => {
+test("return error", (t) => {
   const error = wrap({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
@@ -50,10 +49,10 @@ test("wrap: return error", () => {
   });
 
   const message = "world is not defined";
-  expect(error?.["message"]).toBe(message);
+  t.is(error?.["message"], message);
 });
 
-test("wrap: async return error", async () => {
+test("async return error", async (t) => {
   const error = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(
@@ -68,5 +67,5 @@ test("wrap: async return error", async () => {
   });
 
   const message = "world is not defined";
-  expect(error?.["message"]).toBe(message);
+  t.is(error?.["message"], message);
 });
